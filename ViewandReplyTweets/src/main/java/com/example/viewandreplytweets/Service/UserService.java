9c0343c0fb3a6fafd5c35972129de7493be2613a
Manu khurana <manukhurana10397@gmail.com>
2021-05-18 19:39:17 +0530

    test

--- ViewandReplyTweets/src/main/java/com/example/viewandreplytweets/Service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.viewandreplytweets.Service;
-
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
-
-public interface UserService {
-
-    public Map.Entry<String, String> usercall(String token) throws ExecutionException, InterruptedException;
-}
