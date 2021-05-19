6986884b1783cef2b598f82c3ae92023d6e3205a
Manu khurana <38296197+Manukhurana97@users.noreply.github.com>
2021-05-19 11:46:43 +0530

    Update path-gaurd.guard.ts

--- Frontend/src/app/Guard/path-gaurd.guard.ts
+++ Frontend/src/app/Guard/path-gaurd.guard.ts
@@ -1,7 +1,9 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import {Observable} from 'rxjs';
 import {UserserviceService} from '../Service/userservice.service';
+import {tap} from 'rxjs/operators';
+import {any} from 'codelyzer/util/function';
 
 @Injectable({
   providedIn: 'root'
@@ -10,20 +12,31 @@ export class PathGaurdGuard implements CanActivate {
 
   public check: boolean;
 
-  constructor(private userservice: UserserviceService) {
+  constructor(private userservice: UserserviceService, private route: Router) {
   }
 
   canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    this.userservice.CheckToken().subscribe(
-      data => {
-        console.log('false');
-        this.check = false;
-      },
-      error => {
-        console.log('true');
-        this.check = true;
-      }
-    );
-    return this.check;
+    return new Observable<boolean>(obs => {
+      this.userservice.CheckToken().subscribe(
+        data => {
+          if (data.status === 200 && data.username !== '') {
+            this.route.navigate(['/']);
+            obs.next(false);
+            console.log(obs);
+          }
+          else {
+            obs.next(true);
+            console.log(obs);
+          }
+        },
+        error => {
+          obs.next(true);
+          console.log(obs);
+        }
+      );
+    });
   }
+
+
+
 }
